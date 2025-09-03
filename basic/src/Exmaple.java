7eb7a7e6c892ba4202f92704eea114afb71e6586
ram kumar <rr697144@gmail.com>
2025-09-03 20:40:36 +0530

    java first program

---
+++ basic/src/Exmaple.java
@@ -0,0 +1,14 @@
+public class Exmaple {
+
+    public static void main(String[] args) {
+        System.out.println("This is java first program");
+        System.out.println("Bharat mata ki jay");
+        System.out.println(3 + 3);
+
+        for(int i = 0; i<10; i++){
+            System.out.print(i + " ");
+        }
+        
+    }
+
+}

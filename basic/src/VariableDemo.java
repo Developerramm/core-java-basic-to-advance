fe097fa9311c95c2114792f03cca02cb1dc9f816
ram kumar <rr697144@gmail.com>
2025-09-04 09:10:36 +0530

    variable and data type

---
+++ basic/src/VariableDemo.java
@@ -0,0 +1,32 @@
+public class VariableDemo {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+
+        byte number1 = 20;
+        System.out.println(number1);
+
+        short number2 = 129;
+        System.out.println(number2);
+
+        int age = 30;
+        System.out.println(age);
+
+        long number3 = 434234242423l;
+        System.out.println(number3);
+
+        float x = 343.343f;
+        double y = 4342425.43243243d;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        boolean isProgrammer = true;
+        System.out.println(isProgrammer);
+
+        char ch = 'A';
+        System.out.println(ch);
+        System.out.println((char) ch + 1);
+    }
+
+}

55045e291d5b069767358d74c362031249ac2b20
ram kumar <rr697144@gmail.com>
2025-09-04 09:59:38 +0530

    if else all

---
+++ basic/src/IfElse1.java
@@ -0,0 +1,16 @@
+public class IfElse1 {
+
+    public static void main(String[] args) {
+        int number = 0;
+
+        if (number > 0) {
+            System.out.println("Number is positive here ");
+        } else if (number < 0) {
+            System.out.println("Number is negative here ");
+
+        } else {
+            System.out.println("Number is zero here ");
+        }
+    }
+
+}
---
+++ basic/src/IfElseDemo.java
@@ -0,0 +1,19 @@
+public class IfElseDemo {
+
+    public static void main(String[] args) {
+        int age = 40;
+
+        if (age >= 18) {
+            System.out.println("You can vote now");
+        } else {
+            System.out.println("You cannot vote now ");
+        }
+
+        String language = "Java";
+
+        if (language == "Java") {
+            System.out.println("Best Programming Language");
+        }
+    }
+
+}

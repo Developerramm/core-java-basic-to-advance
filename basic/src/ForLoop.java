99b70433ed3e2e4b81b38b693da1db4316799a83
ram kumar <rr697144@gmail.com>
2025-09-04 10:19:18 +0530

    for loop

---
+++ basic/src/ForLoop.java
@@ -0,0 +1,22 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        // print counting using loop
+        for (int i = 0; i < 100; i++)
+            System.out.print(i + 1 + " ");
+
+
+        // print name using loop    
+        String name = "ram kumar";
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(name);
+        }
+
+        // print table
+        for(int i = 1; i<=10; i++){
+            System.out.println( 5 + " * " + i + " = " + 5 * i);
+        }
+    }
+
+}

7c9c9811fc29e489a5882cb3703be9e46ae1407a
ram kumar <rr697144@gmail.com>
2025-09-29 19:43:05 +0530

    sum of two number

---
+++ javapractice/src/basic/SumOfTwoNumber.java
@@ -0,0 +1,25 @@
+package basic;
+
+import java.util.Scanner;
+
+public class SumOfTwoNumber {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        int number1 = input.nextInt();
+        System.out.print("Enter second number : ");
+        int number2 = input.nextInt();
+
+        int sum = number1 + number2;
+
+        System.out.println("The sum of two number is " + sum);
+
+        input.close();
+
+        
+    }
+
+}

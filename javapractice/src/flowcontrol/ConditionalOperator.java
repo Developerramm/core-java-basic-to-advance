22acd0a07b292e878625bf06c6e53fe892d45b32
ram kumar <rr697144@gmail.com>
2025-10-28 20:28:06 +0530

    conditional operator

---
+++ javapractice/src/flowcontrol/ConditionalOperator.java
@@ -0,0 +1,18 @@
+package flowcontrol;
+
+import java.util.Scanner;
+
+public class ConditionalOperator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+
+        if (value % 2 == 0 && value % 3 == 0) {
+            System.out.println("found ans " + value);
+        }
+
+        sc.close();
+    }
+
+}
